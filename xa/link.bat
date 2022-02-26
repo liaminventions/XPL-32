edaa4c247a2e731cba76e47c0b20f2cb9d4ea73a
liaminventions <unstinkable919@gmail.com>
2022-02-25 22:22:44 -0500

    best linker!11!!1!11!!!!

--- xa/link.bat
+++ xa/link.bat
@@ -1,5 +1,14 @@
+set /p msg= "Commit Message: "
 copy xpl.BIN ..\osi_basic_master
 cd ..\osi_basic-master
-assemble.bat
-5
+ca65 osi_bas.s -o osi_bas.o -l
+ld65 -C osi_bas.cfg osi_bas.o -o osi_bas.bin
+ca65 "osi_bas - Copy.s" -o "osi_bas - Copy.o" -l
+ld65 -C "osi_bas - Copy.cfg" "osi_bas - Copy.o" -o "osi_bas - Copy.bin"
+COPY /B osi_bas.bin + xpl.BIN ROM.BIN
+cd ..
+git add *
+git status
+git commit -m "%msg%"
+git push -u orgin main
 pause
