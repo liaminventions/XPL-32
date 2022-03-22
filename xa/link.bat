7c184926e23d6fe676f9099840a39af77be9605d
liaminventions <unstinkable919@gmail.com>
2022-03-21 21:40:39 -0400

    Startup sound?!?!? werk plz

--- xa/link.bat
+++ xa/link.bat
@@ -1,7 +1,7 @@
-@echo off
+
 set /p msg= "Commit Message: "
-copy xpl.BIN ..\osi_basic_master
-cd ..\osi_basic-master
+copy xpl.BIN ..\osi_basic-master\
+cd ..\osi_basic-master\
 ca65 osi_bas.s -o osi_bas.o -l
 ld65 -C osi_bas.cfg osi_bas.o -o osi_bas.bin
 ca65 "osi_bas - Copy.s" -o "osi_bas - Copy.o" -l
