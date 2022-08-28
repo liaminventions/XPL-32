43b8c196fb39e9dade511ec8bd0417d174aa5aef
Liam <unstinkable919@gmail.com>
2022-08-28 19:32:39 +0100

    ok that is done, now i will make the sd work with basic

--- osi_basic-master/asm.sh
+++ osi_basic-master/asm.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-ca65 osi_bas.s -o osi_bas.o -l
+ca65 osi_bas.s -o osi_bas.o -l a.list
 ld65 -C osi_bas.cfg osi_bas.o -o osi_bas.bin
 
-ca65 "osi_bas - Copy.s" -o "osi_bas - Copy.o" -l
+ca65 "osi_bas - Copy.s" -o "osi_bas - Copy.o" -l b.list
 ld65 -C "osi_bas - Copy.cfg" "osi_bas - Copy.o" -o "osi_bas - Copy.bin"
 
 cd ../xa
