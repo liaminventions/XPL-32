1774fabf465b2a6acc5600dd43a7fb956df9d925
liaminventions <unstinkable919@gmail.com>
2022-08-28 13:32:38 -0400

    ok there ye go

--- osi_basic-master/asm.sh
+++ osi_basic-master/asm.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
+ca65 osi_bas.s -o osi_bas.o -l
+ld65 -C osi_bas.cfg osi_bas.o -o osi_bas.bin
 
+ca65 "osi_bas - Copy.s" -o "osi_bas - Copy.o" -l
+ld65 -C "osi_bas - Copy.cfg" "osi_bas - Copy.o" -o "osi_bas - Copy.bin"
+
+cd ../xa
+./link.sh
