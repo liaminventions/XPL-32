ad404ad0216e2a60f9d1b180631fb72dc6b63bb8
Waverider <unstinkable919@gmail.com>
2022-09-26 17:11:49 -0400

    added the list file so I can debug (finally)

--- vasm6502/asm.sh
+++ vasm6502/asm.sh
@@ -3,4 +3,4 @@ echo "Input: "
 read IN
 echo "Output: "
 read OUT
-./vasm6502_oldstyle -Fbin -dotdir -c02 $IN -o /mnt/usb/folder/$OUT
+./vasm6502_oldstyle -Fbin -dotdir -c02 $IN -o /mnt/usb/folder/$OUT -L a.list
