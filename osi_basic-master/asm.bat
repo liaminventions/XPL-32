615c7ad969b5cd45db26f78d8f458cd513a82504
Waverider <unstinkable919@gmail.com>
2022-09-26 21:27:56 -0400

    WHART a.LIST is gone!>>!>

--- osi_basic-master/asm.bat
+++ osi_basic-master/asm.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p input= "Input File: "
 set /p output= "Output File: "
-vasm6502_oldstyle -Fbin -dotdir -c02 "%input%" -o "%output%" -L a.lst
+vasm6502_oldstyle -Fbin -dotdir -c02 "%input%" -o "%output%" -L a.list
