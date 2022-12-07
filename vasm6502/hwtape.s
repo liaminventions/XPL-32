716f74326c70d9b31ce30b1d1d53d053aaa77244
Liam <unstinkable919@gmail.com>
2022-12-07 19:59:58 +0000

    updated tap.s

--- vasm6502/hwtape.s
+++ vasm6502/hwtape.s
@@ -3,9 +3,9 @@ PORTA = $b001
 DDRB = $b002
 DDRA = $b003
 
-LCD_E  = %10000000
-LCD_RW = %01000000
-LCD_RS = %00100000
+;LCD_E  = %10000000
+;LCD_RW = %01000000
+;LCD_RS = %00100000
 
 SD_CS    = %00010000
 SD_SCK   = %00001000
@@ -19,7 +19,7 @@ PORTA_OUTPUTPINS = TAPE_EAR | TAPE_MIC
 via_init:
   lda #%11111111          ; Set all pins on port B to output
   sta DDRB
-  lda #%10111111   ; Set various pins on port A to output
+  lda #PORTA_OUTPUTPINS   ; Set various pins on port A to output
   sta DDRA
   rts
 
