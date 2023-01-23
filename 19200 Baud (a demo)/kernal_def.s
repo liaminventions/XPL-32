fa6ce8cc11663c9f5a3f0fc9bd6bfd41c1a027ee
Waverider <unstinkable919@gmail.com>
2023-01-23 16:31:50 -0500

    kernal

--- 19200 Baud (a demo)/kernal_def.s
+++ 19200 Baud (a demo)/kernal_def.s
@@ -1,50 +1,54 @@
 ; kernal def
 
-acia_init = $ffb2
+acia_init = $ffac
 
-stock_via = $ffb5
+stock_via = $ffaf
 
-sd_init = $ffb8
+sd_init = $ffb2
 
-fat32_init = $ffbb
+fat32_init = $ffb5
 
-print_hex_acia = $ffbe
+print_hex_acia = $ffb8
 
-crlf = $ffc1
+crlf = $ffbb
 
-cleardisplay = $ffc4
+cleardisplay = $ffbe
 
-rxpoll = $ffc7
+rxpoll = $ffc1
 
-txpoll = $ffca
+txpoll = $ffc4
 
-print_chara = $ffcd
-print_char_acia = $ffcd
+print_chara = $ffc7
+print_char_acia = $ffc7
 
-ascii_home = $ffd0
+ascii_home = $ffca
 
-w_acia_full = $ffd3
+w_acia_full = $ffcd
 
-fat32_seekcluster = $ffd6
+fat32_seekcluster = $ffd0
 
-fat32_readnextcluster = $ffd9
+fat32_readnextcluster = $ffd3
 
-fat32_openroot = $ffdc
+fat32_openroot = $ffd6
 
-fat32_opendirent = $ffdf
+fat32_opendirent = $ffd9
 
-fat32_readdirent = $ffe2
+fat32_readdirent = $ffdc
 
-fat32_finddirent = $ffe5
+fat32_finddirent = $ffdf
 
-fat32_readbyte = $ffe8
+fat32_file_readbyte = $ffe2
 
-fat32_file_read = $ffeb
+fat32_file_read = $ffe5
 
-sd_readbyte = $ffee
+sd_readbyte = $ffe8
 
-sd_sendcommand = $fff1
+sd_sendcommand = $ffeb
 
-sd_readsector = $fff4
+sd_readsector = $ffee
 
-loadcmd = $fff7
+loadcmd = $fff1
+
+tsave = $fff4
+
+tload = $fff7
