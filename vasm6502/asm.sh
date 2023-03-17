38f359f4a1181a31f222a4287588d3a7bf83873d
Waverider <unstinkable919@gmail.com>
2023-03-16 22:30:13 -0400

    changes to asm.sh and rename tms_groove

--- vasm6502/asm.sh
+++ vasm6502/asm.sh
@@ -3,4 +3,4 @@ echo "Input: "
 read IN
 echo "Output: "
 read OUT
-./vasm6502_oldstyle -Fbin -dotdir -c02 $IN -o /mnt/usb/folder/$OUT -L a.list
+vasm6502_oldstyle -Fbin -dotdir -c02 $IN -o /run/media/$USER/XPL/root/$OUT -L a.list
